64ce574d7dfb544de23355e8ebb0d1883feb0ba9
hhsadiq <hhsadiq@gmail.com>
2017-10-08 07:58:22 +0500

    Cleaning of chapter one

--- src/chapter1/ch1-q1.js
+++ src/chapter1/ch1-q1.js
@@ -10,16 +10,7 @@
  * @param  {string[]} str String to check, passed in as a character array
  * @return {boolean}      True if unique characters, otherwise false
  */
-export function hasUniqueCharactersSet(str) {
-  let chars = new Set();
-
-  for (let i = 0; i < str.length; ++i) {
-    if (chars.has(str[i])) {
-      return false;
-    }
-    chars.add(str[i]);
-  }
-  return true;
+export function hasUniqueCharactersSet() {
 }
 
 /**
@@ -33,14 +24,5 @@ export function hasUniqueCharactersSet(str) {
  * @param  {string[]} str String to check, passed in as a character array
  * @return {boolean}      True if unique characters, otherwise false
  */
-export function hasUniqueCharactersSort(str) {
-  // sort string using quicksort
-  str.sort();
-
-  for (var i = 1; i < str.length; ++i) {
-    if (str[i] === str[i - 1]) {
-      return false;
-    }
-  }
-  return true;
+export function hasUniqueCharactersSort() {
 }
