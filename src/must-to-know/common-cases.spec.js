33ac41d3b976ef4acb87db46708270a286471e7c
hhsadiq <hhsadiq@gmail.com>
2017-10-12 11:40:21 +0500

    basic work on tree

--- src/must-to-know/common-cases.spec.js
+++ src/must-to-know/common-cases.spec.js
@@ -60,3 +60,104 @@ export const sortingCases = [
     order: 'dEsC'
   }
 ];
+
+export const tree = {
+  value: 1,
+  children: [
+    {
+      value: 2,
+      children: [
+        {
+          value: 3,
+          children: [
+          ]
+        },
+        {
+          value: 4,
+          children: [
+          ]
+        },
+        {
+          value: 5,
+          children: [
+            {
+              value: 6,
+              children: [
+              ]
+            },
+            {
+              value: 7,
+              children: [
+              ]
+            },
+            {
+              value: 8,
+              children: [
+              ]
+            }
+          ]
+        },
+        {
+          value: 9,
+          children: [
+            {
+              value: 10,
+              children: [
+              ]
+            }
+          ]
+        },
+        {
+          value: 11,
+          children: [
+          ]
+        }
+      ]
+    },
+    {
+      value: 12,
+      children: [
+        {
+          value: 13,
+          children: [
+            {
+              value: 14,
+              children: [
+                {
+                  value: 15,
+                  children: [
+                  ]
+                },
+                {
+                  value: 16,
+                  children: [
+                  ]
+                },
+                {
+                  value: 17,
+                  children: [
+                  ]
+                },
+                {
+                  value: 18,
+                  children: [
+                  ]
+                },
+                {
+                  value: 19,
+                  children: [
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          value: 20,
+          children: [
+          ]
+        }
+      ]
+    }
+  ]
+};
