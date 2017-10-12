216fb429b2e17e53d6f3dfb753bf0db30df6e1f7
hhsadiq <hhsadiq@gmail.com>
2017-10-12 11:40:21 +0500

    basic work on tree

---
+++ src/must-to-know/tree.spec.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import {
+  Tree
+} from './tree';
+import { sampleTree } from './common-cases.spec';
+
+describe('sampleTree', function () {
+  it('should correctly generate a sampleTree', () => {
+    expect(new Tree(sampleTree.value, sampleTree.children))
+      .to
+      .eql(sampleTree);
+  });
+
+  it('should allow correct breadth first search', () => {
+    const myTree = new Tree(sampleTree.value, sampleTree.children);
+    const traversalResult = [];
+    const BFSIterator = (value) => {
+      traversalResult.push(value);
+    };
+    myTree.breadthFirstSearch(BFSIterator);
+    expect(traversalResult)
+      .to
+      .eql([1, 2, 12, 3, 4, 5, 9, 11, 13, 20, 6, 7, 8, 10, 14, 15, 16, 17, 18, 19]);
+  });
+});
