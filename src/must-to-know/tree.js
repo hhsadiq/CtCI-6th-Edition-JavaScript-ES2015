847a8058f418a70716d782c2f7e85e4ac079f3d1
hhsadiq <hhsadiq@gmail.com>
2017-10-13 09:44:50 +0500

    minimum of path sums

--- src/must-to-know/tree.js
+++ src/must-to-know/tree.js
@@ -1,14 +1,12 @@
-import {Queue} from './queue';
+import { Queue } from './queue';
 
 export class Tree {
   /**
    * Generate the tree from JSON like structure
-   * @param value
-   * @param children
+   * @param root
    */
-  constructor(value, children = []) {
-    this.value = value;
-    this.children = children;
+  constructor(root) {
+    this.root = root;
   }
 
   /**
@@ -17,8 +15,8 @@ export class Tree {
    */
   breadthFirstTraversal(callback) {
     const q = new Queue();
-    callback(this.value);
-    this.children.forEach((node) => q.enqueue(node));
+    callback(this.root.value);
+    this.root.children.forEach((node) => q.enqueue(node));
     while (!q.isEmpty()) {
       const node = q.dequeue();
       callback(node.value);
@@ -31,14 +29,83 @@ export class Tree {
    * @param callback
    */
   depthFirstTraversal(callback) {
-    const q = new Queue();
-    callback(this.value);
-    this.children.forEach((node) => q.enqueue(node));
-    while (!q.isEmpty()) {
-      const node = q.dequeue();
+    this._depthFirstTraversal(
+      this.root,
+      callback
+    );
+  }
+
+  /**
+   * @param node
+   * @param callback
+   * @private
+   */
+  _depthFirstTraversal(node, callback) {
+    if (node.children < 1) {
       callback(node.value);
-      node.children.forEach((value) => q.enqueue(value));
+      return;
     }
+    node.children.forEach(child => this._depthFirstTraversal(child, callback));
+    callback(node.value);
+  }
+
+  sumOfEachPath() {
+    return this._sumOfEachPath(this.root);
+  }
+
+  _sumOfEachPath(node, arr = [], parentSum = 0) {
+    if (node.children < 1) {
+      arr.push(parentSum + node.value);
+      return;
+    }
+    parentSum += node.value;
+    node.children.forEach(child =>
+      this._sumOfEachPath(child, arr, parentSum));
+    return arr;
+  }
+
+  /**
+   * https://www.pramp.com/challenge/15oxrQx6LjtQj9JK9XqA
+   * @returns {*}
+   */
+  minimumOfPathSums() {
+    return this._minimumOfPathSumsPrampSolution(this.root);
+  }
+
+  /**
+   * @param node
+   * @param min
+   * @param parentSum
+   * @returns {*}
+   * @private
+   */
+  _minimumOfPathSums(node, min = [Infinity], parentSum = 0) {
+    if (node.children < 1) {
+      const pathSum = parentSum + node.value;
+      min [0] = pathSum < min[0] ? pathSum : min[0];
+      return;
+    }
+    parentSum += node.value;
+    node.children.forEach(child =>
+      this._minimumOfPathSums(child, min, parentSum));
+    return min[0];
+  }
+
+  /**
+   * @param node
+   * @returns {*}
+   * @private
+   */
+  _minimumOfPathSumsPrampSolution(node) {
+    if (node.children < 1) {
+      return node.value;
+    }
+    let min = Infinity;
+    node.children.forEach(child => {
+      const tempCost = this._minimumOfPathSumsPrampSolution(child);
+      min = tempCost < min ? tempCost : min;
+    });
+    return min + node.value;
   }
 
 }
