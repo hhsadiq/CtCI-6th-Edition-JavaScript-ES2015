1d57e02727818d798e50bf0b1571a4a01a8fffa9
hhsadiq <hhsadiq@gmail.com>
2017-10-13 11:01:50 +0500

    Dynamic tests

--- src/must-to-know/tree.spec.js
+++ src/must-to-know/tree.spec.js
@@ -2,46 +2,44 @@ import { expect } from 'chai';
 import {
   Tree
 } from './tree';
-import { sampleTree } from './common-cases.spec';
+import { treeCases } from './common-cases.spec';
 
-describe('tree', function () {
+describe('tree', () => {
   it('Generate the tree', () => {
-    expect(new Tree(sampleTree).root)
+    expect(new Tree(treeCases[0].tree).root)
       .to
-      .eql(sampleTree);
+      .eql(treeCases[0].tree);
   });
 
-  const myTree = new Tree(sampleTree);
-  let traversalResult = [];
-  const iterator = (value) => {
-    traversalResult.push(value);
-  };
 
-  it('Breadth first search', () => {
-    traversalResult = [];
-    myTree.breadthFirstTraversal(iterator);
-    expect(traversalResult)
-      .to
-      .eql([1, 2, 12, 3, 4, 5, 9, 11, 13, 20, 6, 7, 8, 10, 14, 15, 16, 17, 18, 19]);
-  });
+  treeCases.forEach((context, i) => {
+    const myTree = new Tree(context.tree);
+    it(`Breadth first search in tree ${i}`, () => {
+      let traversalResult = [];
+      myTree.breadthFirstTraversal(value => traversalResult.push(value));
+      expect(traversalResult)
+        .to
+        .eql(context.BFT);
+    });
 
-  it('Depth first search', () => {
-    traversalResult = [];
-    myTree.depthFirstTraversal(iterator);
-    expect(traversalResult)
-      .to
-      .eql([3, 4, 6, 7, 8, 5, 10, 9, 11, 2, 15, 16, 17, 18, 19, 14, 13, 20, 12, 1]);
-  });
+    it(`Depth first search in tree ${i}`, () => {
+      let traversalResult = [];
+      myTree.depthFirstTraversal(value => traversalResult.push(value));
+      expect(traversalResult)
+        .to
+        .eql(context.DFT);
+    });
 
-  it('Should return sum of each path', () => {
-    expect(myTree.sumOfEachPath())
-      .to
-      .eql([6, 7, 14, 15, 16, 22, 14, 55, 56, 57, 58, 59, 33]);
-  });
+    it(`Should return sum of each path in tree ${i}`, () => {
+      expect(myTree.sumOfEachPath())
+        .to
+        .eql(context.sumOfEachPath);
+    });
 
-  it('Should return minimum of path sums', () => {
-    expect(myTree.minimumOfPathSums())
-      .to
-      .eql(6);
+    it(`Should return minimum path in tree ${i}`, () => {
+      expect(myTree.minPath())
+        .to
+        .eql(context.minPath);
+    });
   });
 });
